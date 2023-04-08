8f7921e608f086cecc35bbacdc1aedcee5a83485
Wendirad Demelash <wendiradame@gmail.com>
2023-04-08 01:43:01 -0700

    (feat) Implement `run` function

--- tv_recording/cli.py
+++ tv_recording/cli.py
@@ -3,6 +3,10 @@ A command line interface for tv_recording.
 """
 import argparse
 import logging
+import subprocess
+from pathlib import Path
+
+from .config import Config
 
 
 class CLI:
@@ -18,9 +22,21 @@ class CLI:
         """
         self.args = args
         self.logger = logger or logging.getLogger(__name__)
+        self.config = Config(args.config, logger)
 
     def run(self):
         """
         Main entry point.
         """
         self.logger.info("tv_recording.cli.CLI.run()")
+        path = Path(self.args.output)
+        command = [
+            "ffmpeg",
+            *self.config.ffmpeg_args,
+            path.absolute().as_posix(),
+        ]
+        process = subprocess.Popen(command)
+
+        logging.info("Recording to %s", path.absolute().as_posix())
+        # Wait for the process to finish
+        process.communicate()
